7af11087c23920f0f514b5add12ce503ea2982fb
SebG-prog <sebastien-gruet@orange.fr>
2020-04-13 15:43:33 +0200

    Created a GameSessionInterface.css file, and displaying several boxes related to the song title that is to find

--- src/components/GameSessionInterface.js
+++ src/components/GameSessionInterface.js
@@ -1,7 +1,30 @@
 import React from "react"
-
+import "./GameSessionInterface.css"
 class GameSessionInterface extends React.Component {
 
+    state = {
+        title: "Let it be",
+        solution: ""
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="solutionDisplayBoxes">
+                    {
+                        this.state.title.split(" ")
+                            .map((word) => (
+                                <div className="word">
+                                    {word.split("").map(() =>
+                                        <div className="letter"></div>
+                                    )}
+                                </div>
+                            ))
+                    }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default GameSessionInterface
