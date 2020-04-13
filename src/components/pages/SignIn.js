d146e0a67e7f26043565c3862192bd8328cbd7cb
Claire Kodia <claire.kodia@gmail.com>
2020-04-13 12:36:31 +0200

    Added a class and h1 to the demo pages Home, ThemePage, Sign in and Contact Pages

---
+++ src/components/pages/SignIn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello SignIn Page</h1>
+            </div>
+        );
+    }
+}
+export default SignIn;
---
+++ src/components/pages/ThemePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ThemePage extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello Theme display Page</h1>
+            </div>
+        );
+    }
+}
+export default ThemePage;
