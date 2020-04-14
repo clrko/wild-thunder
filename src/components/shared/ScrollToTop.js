e137a92548ecd57ac928aab72f5ce305648ca8c0
Claire Kodia <claire.kodia@gmail.com>
2020-04-14 14:50:00 +0200

    Saved

---
+++ src/components/shared/ScrollToTop.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class SrollToTop extends Component {
+    constructor(props) {
+
+    }
+
+    componentDidMount() {
+
+    }
+
+    toggleVisibility() {
+
+    }
+
+    scrollToTop() {
+
+    }
+
+    render() {
+        <div className="scroll-to-top">
+          <div>
+           
+          </div>
+      </div>
+
+    }
+}
