8901d96a90ee0e9f1fc1aeb6b147fb361c596566
Claire Kodia <claire.kodia@gmail.com>
2020-04-14 16:03:27 +0200

    imported the scroll button component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Test from "./components/pages/Test";
+import ScrollToTop from "./components/shared/ScrollToTop";
 
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <ScrollToTop />
     </div>
   );
 }
