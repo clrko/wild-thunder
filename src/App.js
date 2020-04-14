158475356c6a937c523a3b2fab278a3b56be9bcf
Claire Kodia <claire.kodia@gmail.com>
2020-04-14 10:59:25 +0200

    Put the scroll button on hold

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import NavbarHeader from './components/shared/NavbarHeader';
+import NavbarFooter from './components/shared/NavbarFooter';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
 
 import ThemePage from './components/pages/ThemePage';
 import SignIn from './components/pages/SignIn';
 import Contact from './components/pages/Contact';
+
 
 class App extends Component {
   render() {
@@ -12,6 +15,7 @@ class App extends Component {
       <BrowserRouter>
       <div className="App">
         <NavbarHeader/>
+        <NavbarFooter/>
         <Switch>
           <Route exact path="/" component={ThemePage}/>
           {/* see how to make a link to the modal page */}
--- src/components/shared/NavbarFooter.js
+++ src/components/shared/NavbarFooter.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
+/* import { faArrowUp } from '@fortawesome/free-solid-svg-icons' */
 
 import "./Navbar.css"
 
