609ebafd07cc59c908c62288ee6195c9246e685d
SebG-prog <sebastien-gruet@orange.fr>
2020-04-16 17:07:25 +0200

    Added a method to get artist tracks list from artistID with axios

--- src/components/pages/GameSession.js
+++ src/components/pages/GameSession.js
@@ -6,7 +6,38 @@ const API_KEY = "ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm"
 const genresCode = "g.5"
 
 class GameSession extends React.Component {
+
+    state = {
+        artistsList: [],
+        artistTracks: []
+    }
+
+    getArtistsList = (genresCode) => {
+        axios.get(`http://api.napster.com/v2.2/genres/${genresCode}/artists/top?apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4&limit=10`)
+            .then(res => {
+                console.log("Artists List: ", res.data) ||
+                this.setState({ artistList: res.data },
+                    () => this.getArtistTracksList(this.state.artistList.artists[0].id))
+            })
+
+        console.log("appelAPI: ", this.state.artistsList)
+    }
+
+    getArtistTracksList = (artistID) => {
+        axios.get(`https://api.napster.com/v2.2/artists/${artistID}/tracks/top?apikey=${API_KEY}`)
+            .then(res => {
+                console.log("Artist Tracks: ", res.data.tracks) ||
+                    this.setState({ artistTracks: res.data.tracks })
+            })
+    }
+
+    componentDidMount() {
+        this.getArtistsList(genresCode)
+    }
+
     render() {
+
+
         return (
             <div className="container">
                 <h1>{"artistName"}</h1>
