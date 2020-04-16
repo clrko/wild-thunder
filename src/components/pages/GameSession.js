4c213a0c7d2f526ebcb08970fd48f8fcc167326d
SebG-prog <sebastien-gruet@orange.fr>
2020-04-16 16:27:27 +0200

    initiating the GameSession.js page

---
+++ src/components/pages/GameSession.js
@@ -0,0 +1,16 @@
+import React from "react"
+import axios from "axios"
+
+const API_KEY = "ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm"
+
+const genresCode = "g.5"
+
+class GameSession extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default GameSession
