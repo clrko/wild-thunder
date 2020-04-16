fb8c47284ca9f08e1292269106b96302c9e202d0
Claire Kodia <claire.kodia@gmail.com>
2020-04-16 10:19:28 +0200

    Added classes to the navlinks

--- src/components/shared/NavbarFooter.js
+++ src/components/shared/NavbarFooter.js
@@ -4,7 +4,6 @@ import {NavLink} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
-/* import { faArrowUp } from '@fortawesome/free-solid-svg-icons' */
 
 import "./Navbar.css"
 
@@ -14,9 +13,8 @@ class NavbarFooter extends Component {
         return(
             <nav className="nav-bottom-container">
                 <ul className="nav-links-wrapper">
-                    <li><NavLink to="/pseudo"><FontAwesomeIcon icon={faHome} /></NavLink></li> {/* must lead to the homepage*/}
-                    <li><NavLink to="/sign_in/pseudo"><FontAwesomeIcon icon={faUser} /></NavLink></li>
-                    {/* <li><NavLink to="/sign_in"><FontAwesomeIcon icon={faArrowUp} /></NavLink></li> */}
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/pseudo"><FontAwesomeIcon icon={faHome} /></NavLink></li>
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/sign_in/pseudo"><FontAwesomeIcon icon={faUser} /></NavLink></li>
                 </ul>
             </nav>
         );
--- src/components/shared/NavbarHeader.js
+++ src/components/shared/NavbarHeader.js
@@ -11,9 +11,9 @@ class NavbarHeader extends Component {
             <nav className="nav-container">
                 <MainLogo />
                 <ul className="nav-links-wrapper">
-                    <li><NavLink to="/log_in/pseudo">Log In</NavLink></li> {/* must open the modal page */}
-                    <li><NavLink to="/sign_in/pseudo">Sign In</NavLink></li>
-                    <li><NavLink to="/contact/pseudo">Contact</NavLink></li>
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/log_in/pseudo">Log In</NavLink></li> {/* must open the modal page */}
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/sign_in/pseudo">Sign In</NavLink></li>
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/contact/pseudo">Contact</NavLink></li>
                 </ul>
             </nav>
         );
