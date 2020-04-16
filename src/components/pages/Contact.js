86c4e492211bd17c277a6dbb0b643b2bb477ad19
Claire Kodia <claire.kodia@gmail.com>
2020-04-16 10:18:30 +0200

    Uldated the title of the page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import NavbarHeader from '../shared/NavbarHeader';
 import NavbarFooter from '../shared/NavbarFooter';
+import './Contact.css'
 
 class SignIn extends Component{
     render(){
