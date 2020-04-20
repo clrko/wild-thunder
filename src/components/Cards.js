fbe21a536c7eb2b5a0f10eae6959f4dc1a16e47e
SebG-prog <sebastien-gruet@orange.fr>
2020-04-20 20:53:09 +0200

    link from themecards to gamesession page is now functional

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import "./Cards.css"
+import { NavLink } from 'react-router-dom';
 
 
 function Cards(props) {
     return (
+        <NavLink to={'/gamesession/pseudo/genre'}>
         <div className="card" style={{background: props.divStyle}}>
             <div className="card_image">
                 <div className="card_title title-white">
@@ -11,9 +13,9 @@ function Cards(props) {
                 </div>
             </div>
         </div>
+        </NavLink>
     );
 }
-
 
 export default Cards
 
