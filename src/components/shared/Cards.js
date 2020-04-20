6f36d1cb818597c174571ff05c965a8b9b29a560
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-04-20 15:38:51 +0200

    Added Cards.js and .css to the repository 'shared'

---
+++ src/components/shared/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Cards.css"
+
+
+function Cards(props) {
+    return (
+        <div className="card" style={props.divStyle}>
+            <div className="card_image">
+                <div className="card_title title-white">
+                    <div>{props.title}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Cards
+
