5feb43f84f64d63fcaf6de9bd348ab727b39357f
Claire Kodia <claire.kodia@gmail.com>
2020-04-21 16:55:02 +0200

    Created the End Session page

---
+++ src/components/pages/EndSession.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+import EndSessionTrackList from  "./EndSessionTrackList"
+
+class EndSession extends Component {
+    render() {
+        return (
+            <EndSessionTrackList />
+        )
+    }
+}
+
+export default EndSession
