52c820efd5760abe19f9e0aa9675574a522d9a1d
Claire Kodia <claire.kodia@gmail.com>
2020-04-23 09:21:35 +0200

    Managed to link the handleToggleClick method to the right link

--- src/components/pages/EndSessionTrackList.js
+++ src/components/pages/EndSessionTrackList.js
@@ -1,16 +1,44 @@
-import React from "react"
+import React, {Component} from "react"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faPause } from '@fortawesome/free-solid-svg-icons'
+/* import { faVolumeUp } from '@fortawesome/free-solid-svg-icons' */
 
 
-const EndSessionTrackList = props => {
-    return (
-        <div>
-            <p>{props.sessionHistory.name}</p>
-            <p>{props.sessionHistory.artistName}</p>
-            <audio id="audioPlayer" src={props.sessionHistory.previewURL} controls>
-                <source type="audio/mpeg" />
-            </audio>
-        </div>
-    )
+class EndSessionTrackList extends Component {
+
+    state = {
+        isPaused: true
+    }
+
+    
+
+    handleToggleClick = () => {
+
+        const myAudio = document.getElementById(this.props.sessionHistory.id)
+        
+        if (myAudio.paused) {
+            this.setState({isPaused: !this.state.isPaused}) 
+            myAudio.play();
+        } else { 
+            myAudio.pause();
+            this.setState({isPaused: !this.state.isPaused}) 
+        } 
+    }
+
+    render() {
+        return (
+            <div>
+                <p>{this.props.sessionHistory.name}</p>
+                <p>{this.props.sessionHistory.artistName}</p>
+                <audio id={this.props.sessionHistory.id} src={this.props.sessionHistory.previewURL} controls>
+                    <source type="audio/mpeg" />
+                </audio>
+                <button onClick={this.handleToggleClick}><FontAwesomeIcon icon={this.state.isPaused? faPlay : faPause }/></button>
+            </div>
+        )
+    }
 }
 
 export default EndSessionTrackList
