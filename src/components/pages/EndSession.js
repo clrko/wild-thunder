2c1251def86e2432a35add83046f2c3923990c4d
Claire Kodia <claire.kodia@gmail.com>
2020-04-23 14:59:01 +0200

    Improved the style

--- src/components/pages/EndSession.js
+++ src/components/pages/EndSession.js
@@ -8,7 +8,7 @@ class EndSession extends Component {
     render() {
         console.log("4propslcoation state:", this.props.location.state)
         return (
-            <div>
+            <div className="endsession-container">
                 <h1>The tracklist of your game session :</h1>
                 {this.props.location.state.map(track => <EndSessionTrackList sessionHistory={track} />)}
             </div>
