f6372124fbc88bed98cd651204e0cb44d6ac6c03
Claire Kodia <claire.kodia@gmail.com>
2020-04-23 14:47:23 +0200

    Styled the title h1

--- src/components/pages/EndSession.js
+++ src/components/pages/EndSession.js
@@ -2,12 +2,14 @@ import React, { Component } from "react"
 
 import EndSessionTrackList from  "./EndSessionTrackList"
 
+import "./EndSession.css"
+
 class EndSession extends Component {
     render() {
         console.log("4propslcoation state:", this.props.location.state)
         return (
             <div>
-                <p>The results of your game session :</p>
+                <h1>The tracklist of your game session :</h1>
                 {this.props.location.state.map(track => <EndSessionTrackList sessionHistory={track} />)}
             </div>
         )
