9ecb0c2bfd6b76b2902477ad39ad3a20e967e22a
Claire Kodia <claire.kodia@gmail.com>
2020-04-26 13:50:13 +0200

    Structured and styled the MainRules component but we are stull missing the cards and some color

---
+++ src/components/pages/MainRules.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react"
+
+import NavbarHeader from '../shared/NavbarHeader';
+import NavbarFooter from '../shared/NavbarFooter';
+import ScrollToTop from '../shared/ScrollToTop';
+
+import "./MainRules.css"
+
+class MainRules extends  Component {
+    render(){
+        return (
+            <div>
+                <NavbarHeader />
+                <NavbarFooter />
+                <div className="rules_container">
+                    <h1>Main Rules</h1>
+                    <div id="rules_thunder">
+                        <h2>What is Thunder?</h2>
+                        <p>Thunder is a music quiz site simple to use and completely free that help you enrich your musical knowledge.<br/><br/>You don't need to register to start a game. </p>
+                    </div>
+                    
+                    <div id="rules_registration">
+                        <h2>Registration</h2>
+                        <p> Registration allows you to fully enjoy a taylored game experience by choosing your profile, accessing personalized recommendations, creating private playlists and appearing in the ranking.
+                        <br/><br/>Here is an overview of the game mode, profiles and ranking.
+                        </p>
+                    </div>
+
+                    <div id="rules_cards">
+
+                    </div>
+                    
+                    <div id="rules_points">
+                        <h2>Points counting</h2>
+                        <ul>
+                            <li>Find the artist : 1 point</li>
+                            <li>Find the title: 0.5 points</li>
+                            <li>Speed Bonus: 0.5 points</li>
+                        </ul>
+                    </div>
+                </div>
+                <ScrollToTop />
+            </div>
+        )
+    }
+}
+
+export default MainRules
