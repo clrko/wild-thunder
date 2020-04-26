e2c61b665f416ed74a42e21c8397c7ebc1db64a8
Claire Kodia <claire.kodia@gmail.com>
2020-04-26 13:02:41 +0200

    Added a route to mainrules

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Contact from './components/pages/Contact';
 import EndSession from './components/pages/EndSession';
 import GameSession from './components/pages/GameSession';
 import HomePage from './components/pages/HomePage';
+import MainRules from './components/pages/MainRules';
 import Page404 from './components/pages/Page404';
 import ThemePage from './components/pages/ThemePage';
 import SignIn from './components/pages/SignIn';
+
 
 class App extends Component {
   render() {
@@ -16,6 +18,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/theme-page" component={ThemePage} />
+          <Route path="/mainrules" component={MainRules} />
           <Route path="/gamesession" component={GameSession} />
           <Route path="/endsession" component={EndSession} />
           <Route path="/sign_in/:pseudo" component={SignIn} />
