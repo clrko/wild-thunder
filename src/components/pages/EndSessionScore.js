1c6cda8e720699fb650a768248e05161dd2a1964
pinto78600 <pintostephane92@hotmail.fr>
2020-05-05 00:07:04 +0200

    add endSessionScore

---
+++ src/components/pages/EndSessionScore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class EndSessionScore extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Your score final :</h1>
+                    <p></p>
+            </div>
+        )
+    }
+}
+export default EndSessionScore
