450569ed1fa1d7d2ead4dd6d5c429d3b15c8c8cd
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-05-04 16:16:28 +0200

    Updated files

--- src/components/pages/ModePage.js
+++ src/components/pages/ModePage.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ModePageCards from "./ModePageCards"
 import "./ModePage.css";
 
 const arrayMode = ['Standard', 'Survival']
 const gamePathname = [`/theme-page`, `/game-session-survival`]
-const modeColor = ["#6a6cd9","#ef1234"]
+const modeColor = ["#6a6cd9","#ff5500"]
+const description = ['10 songs','No songs limit !']
 
 class ModePage extends Component {
     render() {
         return (
-            <div className="modePage-list">
-                {arrayMode.map((mode, i) => <ModePageCards key={i} title={mode} pathname={gamePathname[i]} modeColor={modeColor[i]}/>)}
-            </div>
+            <Fragment>
+                <h1 className="modePage-title">Pick up your mode</h1>
+                <div className="modePage-list">
+                    {arrayMode.map((mode, i) => <ModePageCards key={i} title={mode} pathname={gamePathname[i]} modeColor={modeColor[i]} description={description[i]} />)}
+                </div>
+            </Fragment>
         )
     }
 }
--- src/components/pages/ModePageCards.js
+++ src/components/pages/ModePageCards.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 import "./ModePageCards.css"
 
-const ModePageCards = ({title, pathname, modeColor}) => {
+const ModePageCards = ({title, pathname, modeColor, description}) => {
     return (
         <NavLink to={{pathname}}> 
             <div className="ModePageCards-card" style={{backgroundColor: modeColor}}>
                 <div className="ModePageCards-title">
                     <p>{title}</p>
+                    <p className="ModePageCards-description">{description}</p>
                 </div>
             </div>
         </NavLink>
