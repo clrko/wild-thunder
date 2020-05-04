c54f9c35d50fbf6736fc2aea49cbcc1c8c9bd9e6
pinto78600 <pintostephane92@hotmail.fr>
2020-05-04 21:44:26 +0200

    modif share facebook

--- src/components/pages/EndSessionShare.js
+++ src/components/pages/EndSessionShare.js
@@ -4,32 +4,17 @@ import logoFacebook from "../../images/facebook.png";
 
 
 class EndSessionShare extends React.Component {
-    
-
-
-
-
-
     render(){
-    
-
     return (
-        
-            <div className="endSessionShare" >
-                <button className="endSessionButtonShare"  
+        <div className="endSessionShare" >
+            <button className="endSessionButtonShare"  
                     data-href="https://developers.facebook.com/docs/plugins/"> <a target="_blank" 
                     href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">
                     <img src={logoFacebook}/></a>
-                    
-                </button>
-            </div>
-            
-          
+            </button>
+        </div>
+            )
 
-          
-    )
-
+    }
 }
-}
-
-export default EndSessionShare
+export default EndSessionShare ;
