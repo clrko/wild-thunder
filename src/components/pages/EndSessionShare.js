81fca75c5d356f043362d4e2f7464655ea3770a6
pinto78600 <pintostephane92@hotmail.fr>
2020-05-04 21:20:19 +0200

    share facebook

--- src/components/pages/EndSessionShare.js
+++ src/components/pages/EndSessionShare.js
@@ -2,17 +2,34 @@ import React from 'react'  ;
 import './EndSessionShare.css' ;
 import logoFacebook from "../../images/facebook.png";
 
-class EndSessionShare extends React.Component {
-    render(){
-    return (
-        <h1>
-           <button className="endSessionButtonShare" >
-                <div ><img src={logoFacebook}/></div>
-                <div className="buttonShare" >Share</div>
-           </button>
-        </h1>
-    )
-}}
 
+class EndSessionShare extends React.Component {
+    
+
+
+
+
+
+    render(){
+    
+
+    return (
+        
+            <div className="endSessionShare" >
+                <button className="endSessionButtonShare"  
+                    data-href="https://developers.facebook.com/docs/plugins/"> <a target="_blank" 
+                    href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">
+                    <img src={logoFacebook}/></a>
+                    
+                </button>
+            </div>
+            
+          
+
+          
+    )
+
+}
+}
 
 export default EndSessionShare
