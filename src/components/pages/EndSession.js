14fe18a0aa28d514d15b80fdd456d2b6645d984f
Claire Kodia <claire.kodia@gmail.com>
2020-05-08 11:22:26 +0200

    Lift up the states and functions from EndSessionTrackList to EndSession

--- src/components/pages/EndSession.js
+++ src/components/pages/EndSession.js
@@ -9,11 +9,73 @@ import { faHome } from '@fortawesome/free-solid-svg-icons';
 import "./EndSession.css";
 
 class EndSession extends Component {
+    state = {
+        artistTrack: this.props.location.state.map(track => track.artistTrack), /* array of objects */
+        isPaused: Array(this.props.location.state.length).fill(true),
+        isFavorite: Array(this.props.location.state.length).fill(false),
+        isArtistFound: this.props.location.state.map(track => track.isArtistFound),
+
+        /* albumId: this.props.location.state.map(track => track.artistTrack.albumId),
+        name: this.props.location.state.map(track => track.artistTrack.name),
+        artistName: this.props.location.state.map(track => track.artistTrack.artistName),
+        previewURL: this.props.location.state.map(track => track.artistTrack.previewURL),
+        id: this.props.location.state.map(track => track.artistTrack.id), */
+        
+    }
+
+    handleFavoriteClick = (idTrack) => {
+        const isFavoriteTemp = [...this.state.isFavorite]
+        console.log("isFavoriteTemp", isFavoriteTemp)
+        const index = this.state.artistTrack.findIndex(item => item.id === idTrack)
+        console.log("idTrack", idTrack)
+        console.log("index", index)
+        isFavoriteTemp[index] = !isFavoriteTemp[index]
+        this.setState({isFavorite:isFavoriteTemp}, () => console.log("isFavrotie after", this.state.isFavorite))
+    }
+
+    handleToggleClick = (idTrack) => {
+        /* const myAudio = document.getElementById(this.stateartistTrack.id)
+        
+        if (myAudio.paused) {
+            this.setState({isPaused: !this.state.isPaused}) 
+            myAudio.play();
+        } else { 
+            myAudio.pause();
+            this.setState({isPaused: !this.state.isPaused}) 
+        }  */
+
+        const isPausedTemp = [...this.state.isPaused]
+        console.log("isPausedTemp", isPausedTemp)
+        const index = this.state.artistTrack.findIndex(item => item.id === idTrack)
+        console.log("idTrack", idTrack)
+        console.log("index", index)
+        isPausedTemp[index] = !isPausedTemp[index]
+        this.setState({isPaused:isPausedTemp}, () => console.log("isPaused after", this.state.isPaused))
+    }
+
+    /* 
+    handleToggleClick = () => {
+
+        const myAudio = document.getElementById(this.props.sessionHistory.artistTrack.id)
+        
+        if (myAudio.paused) {
+            this.setState({isPaused: !this.state.isPaused}) 
+            myAudio.play();
+        } else { 
+            myAudio.pause();
+            this.setState({isPaused: !this.state.isPaused}) 
+        } 
+    } */
+
     render() {
+        /* console.log("artistTrack est", this.state.artistTrack)
+        console.log("isArtisFound est", this.state.isArtistFound)
+        console.log("is pauseest", this.state.isPaused)
+        console.log("is favorite est", this.state.isFavorite) */
         return (
             <div className="endsession-container">
                 <h1>The tracklist of your game session :</h1>
-                {this.props.location.state.map((track,i) => <EndSessionTrackList key={i} sessionHistory={track} />)}
+                {this.state.artistTrack.map((track,i) => <EndSessionTrackList key={track.id} albumId={track.albumId} name={track.name} artistName={track.artistName} id={track.id} previewURL={track.previewURL} handleToggleClick={this.handleToggleClick} handleFavoriteClick={this.handleFavoriteClick} isPaused={this.state.isPaused[i]} isFavorite={this.state.isFavorite[i]} isArtistFound={this.state.isArtistFound[i]} />)}
                 <NavLink to="/" className="goHome_button"><button><FontAwesomeIcon icon={faHome} className="goHome_icon" /></button></NavLink>
                 <EndSessionShare/>
             </div>
--- src/components/pages/EndSessionTrackList.js
+++ src/components/pages/EndSessionTrackList.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
@@ -7,48 +7,39 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 import "./EndSessionTrackList.css";
 
-class EndSessionTrackList extends Component {
-
-    state = {
-        isPaused: true,
-        isFavorite:false,
-    }
-
-    handleFavoriteClick = () => {
-        this.setState({isFavorite:!this.state.isFavorite})
-    }
-
-    handleToggleClick = () => {
-
-        const myAudio = document.getElementById(this.props.sessionHistory.artistTrack.id)
-        
-        if (myAudio.paused) {
-            this.setState({isPaused: !this.state.isPaused}) 
-            myAudio.play();
-        } else { 
-            myAudio.pause();
-            this.setState({isPaused: !this.state.isPaused}) 
-        } 
-    }
-
-    render() {
-        return (
-            <div >
-                <div className="track-container" style={this.props.sessionHistory.isArtistFound ? {border: "2px solid #4dff4d"} : {border: "2px solid #ff4d4d"}} >
-                    <img className="img-tracklist" src={`https://api.napster.com/imageserver/v2/albums/${this.props.sessionHistory.artistTrack.albumId}/images/70x70.jpg`} alt="placeholder" />
-                    <div className="track-info">
-                        <p>{this.props.sessionHistory.artistTrack.name}</p> 
-                        <p>{this.props.sessionHistory.artistTrack.artistName}</p>
-                    </div>
-                    <button onClick={this.handleFavoriteClick}><FontAwesomeIcon className={this.state.isFavorite? "favorite-track" : "not-favorite-track"} icon={faHeart}/></button>
-                    <button onClick={this.handleToggleClick}><FontAwesomeIcon id="play-pause-btn" icon={this.state.isPaused? faPlay : faPause }/></button>
+const EndSessionTrackList = ({isArtistFound, albumId, name, artistName, handleFavoriteClick, handleToggleClick, isFavorite, isPaused, id, previewURL}) => {
+    
+    return (
+        <div >
+            <div className="track-container" style={isArtistFound ? {border: "2px solid #4dff4d"} : {border: "2px solid #ff4d4d"}} >
+                <img className="img-tracklist" src={`https://api.napster.com/imageserver/v2/albums/${albumId}/images/70x70.jpg`} alt="placeholder" />
+                <div className="track-info">
+                    <p>{name}</p> 
+                    <p>{artistName}</p>
                 </div>
-                <audio id={this.props.sessionHistory.artistTrack.id} src={this.props.sessionHistory.artistTrack.previewURL}>
-                    <source type="audio/mpeg" />
-                </audio>
+                <button id={id} onClick={() => handleFavoriteClick(id)}><FontAwesomeIcon className={isFavorite? "favorite-track" : "not-favorite-track"} icon={faHeart}/></button>
+                <button id={id} onClick={() => handleToggleClick(id)}><FontAwesomeIcon className="play-pause-btn" icon={isPaused? faPlay : faPause }/></button>
             </div>
-        )
-    }
+            <audio id={id} src={previewURL}>
+                <source type="audio/mpeg" />
+            </audio>
+        </div>
+    )
 }
 
 export default EndSessionTrackList;
+
+{/* <div >
+            <div className="track-container" style={this.props.sessionHistory.isArtistFound ? {border: "2px solid #4dff4d"} : {border: "2px solid #ff4d4d"}} >
+                <img className="img-tracklist" src={`https://api.napster.com/imageserver/v2/albums/${this.props.sessionHistory.artistTrack.albumId}/images/70x70.jpg`} alt="placeholder" />
+                <div className="track-info">
+                    <p>{this.props.sessionHistory.artistTrack.name}</p> 
+                    <p>{this.props.sessionHistory.artistTrack.artistName}</p>
+                </div>
+                <button onClick={this.props.handleFavoriteClick}><FontAwesomeIcon className={this.props.isFavorite? "favorite-track" : "not-favorite-track"} icon={faHeart}/></button>
+                <button onClick={this.props.handleToggleClick}><FontAwesomeIcon id="play-pause-btn" icon={this.props.isPaused? faPlay : faPause }/></button>
+            </div>
+            <audio id={this.props.sessionHistory.artistTrack.id} src={this.props.sessionHistory.artistTrack.previewURL}>
+                <source type="audio/mpeg" />
+            </audio>
+        </div> */}
