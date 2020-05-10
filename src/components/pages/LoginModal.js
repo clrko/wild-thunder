a8160e8970420c3be83d4ebd0d92f1586e03ceae
Claire Kodia <claire.kodia@gmail.com>
2020-05-10 10:00:29 +0200

    Added a handleSubmit function to post and receive data from the backend

--- src/components/pages/LoginModal.js
+++ src/components/pages/LoginModal.js
@@ -1,13 +1,22 @@
 import React , {useState} from "react";
-
+import axios from 'axios'
 import Modal from 'react-modal';
 
 import "./LoginModal.css";
 
 Modal.setAppElement('#root');
 
-function LoginModal() {
+const LoginModal = () => {
     const [modalIsOpen,setModalIsOpen] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault() 
+        axios.post("http:localhost:4242/auth/login", {
+            username: e.target.name.username,
+            password: e.target.name.password
+        }).then(res => {
+            console.log("resultat", res)
+        })
+    }
         return (
             <div className="homepageModal" >
                 <button onClick={() => setModalIsOpen(true)}>Login</button>
@@ -18,11 +27,14 @@ function LoginModal() {
                 }}>
                     <div className="login-modal">
                         <h2 className="title-login-modal" >Login</h2>
-                        <label className="label-login-modal" >Username</label>
-                        <input className="input-login-modal" type="text" name="username" placeholder="Username" />
-                        <label className="label-login-modal" >Password</label>
-                        <input className="input-login-modal" type="password" name="password" placeholder="Password"  /> 
-                        <input className="button-login-modal" type="submit" value="Login"  />
+                        <form onSubmit={handleSubmit}>
+                            <label className="label-login-modal" >Username</label>
+                            <input className="input-login-modal" type="text" name="username" placeholder="Username" />
+                            <label className="label-login-modal" >Password</label>
+                            <input className="input-login-modal" type="password" name="password" placeholder="Password"  /> 
+                            <input className="button-login-modal" type="submit" value="Login"  />
+                        </form>
+                        
                     </div>
                     
                     <button  onClick={() => setModalIsOpen(false)}>Close</button>
