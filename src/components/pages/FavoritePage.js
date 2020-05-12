a3f16daaa2a602fa07740807b57e72d611c8a0f8
Claire Kodia <claire.kodia@gmail.com>
2020-05-12 21:14:50 +0200

    Store the track list in a state

--- src/components/pages/FavoritePage.js
+++ src/components/pages/FavoritePage.js
@@ -1,18 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 const FavoritePage = () => { 
-        axios.get("http://localhost:4242/favorite")
-            .then(res => {
-                console.log(res.data);
-                console.log(res.status);
-                console.log(res.statusText);
-                console.log(res.headers);
-                console.log(res.config);
-            });
-        
+    const [favoriteTrackList, setFavoriteTrackList] = useState([])
 
-    return <div>Hello</div>
+    const getFavoriteTrackList = () =>{
+        console.log("local storage token est", localStorage.getItem("token"))
+    if (localStorage.getItem("token")) {
+        axios.get("http://localhost:4242/favorite", {
+            headers: {
+            'x-access-token': localStorage.getItem("token"),
+            }
+        }).then(res => {
+            console.log("res.data est ", res.data);
+            console.log("res.status est ",res.status);
+            console.log("res.statusText est ",res.statusText);
+            console.log("res.headers est ",res.headers);
+            console.log("res.congi est ",res.config);
+            setFavoriteTrackList(res.data)
+        });
+    } else {
+        console.log("you need to connect")
+    }
+    }
+    
+    useEffect(getFavoriteTrackList(), [])
+
+    return <div>{favoriteTrackList.map(favoriteTrack => <p>{favoriteTrack.track_id}</p>)}</div>
 }
 
 export default FavoritePage;
