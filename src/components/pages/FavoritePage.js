5a7bb36836385cf788c1461faf766c60d8a2c1e4
Claire Kodia <claire.kodia@gmail.com>
2020-05-12 23:15:22 +0200

    Added methods to handle the toggle click and the audio end. Also Managed to get the song from the napster api

--- src/components/pages/FavoritePage.js
+++ src/components/pages/FavoritePage.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
+import FavoriteTracks from "./FavoriteTracks"
+
+import API_KEY from '../../secret'
+
 const FavoritePage = () => { 
     const [favoriteTrackList, setFavoriteTrackList] = useState([])
+    const [isPaused, setIsPaused] = useState([])
+    const [isFavorite, setIsFavorite] = useState([])
+
 
     const getFavoriteTrackList = () => {
         console.log("local storage token est", localStorage.getItem("token"))
@@ -13,19 +20,59 @@ const FavoritePage = () => {
                 }
             }).then(res => {
                 console.log("res.data est ", res.data);
-                setFavoriteTrackList(res.data)
+                console.log("res.dat.map.join ", res.data.map(favoriteTrack => favoriteTrack.track_id).join(","));
+                
+                const allFavorites = res.data.map(favoriteTrack => favoriteTrack.track_id).join(",")
+                axios.get(`https://api.napster.com/v2.2/tracks/${allFavorites}`,
+                    {
+                        params: {
+                            apikey: API_KEY
+                        }
+                    })
+                    .then(res => {
+                        console.log("mon res est ",  res.data.tracks)
+                        setFavoriteTrackList(res.data.tracks)
+                        setIsPaused(Array(res.data.length).fill(true))
+                        setIsFavorite(Array(res.data.length).fill(false))
+                    })
             });
         } else {
             alert("You need to connect")
         }
     }
-    
+
     useEffect(() => getFavoriteTrackList(), [])
+
+    const handleToggleClick = (idtrack) => {
+        const isPausedTemp = isPaused.map(status => true)
+        const currentIndex = favoriteTrackList.findIndex(item => item.id === idtrack)
+        isPausedTemp[currentIndex] = !isPausedTemp[currentIndex]
+        setIsPaused(isPausedTemp)
+
+        const targetAudio = document.getElementById(idtrack)
+        if (targetAudio.paused) {
+            favoriteTrackList.filter(track => track.id !==idtrack).forEach(item => document.getElementById(item.id).pause())
+            targetAudio.play()
+        } else {
+            targetAudio.pause()
+            isPausedTemp[currentIndex] = !isPausedTemp[currentIndex]
+        }
+        
+    }
+
+    const handlePlayEnded = (e) => {
+        const isPausedTemp = [...isPaused]
+        const index = favoriteTrackList.findIndex(item => item.id === e.target.id)
+        isPausedTemp[index] = !isPausedTemp[index]
+        setIsPaused(isPausedTemp)
+    }
 
     return (
         
         <div>
-            {favoriteTrackList.map(favoriteTrack => <p>{favoriteTrack.track_id}</p>)}
+            {console.log("the favorite track list is ", {favoriteTrackList})}
+            {favoriteTrackList.map(favoriteTrack => <p>{favoriteTrack.artistName}</p>)}
+            {favoriteTrackList.map(favoriteTrack => <FavoriteTracks key={favoriteTrack.id} albumId={favoriteTrack.albumId} name={favoriteTrack.name} artistName={favoriteTrack.artistName} /* handleFavoriteClick={handleFavoriteClick} */ handleToggleClick={handleToggleClick} handlePlayEnded={handlePlayEnded} /* isFavorite={isFavorite} */ isPaused={isPaused} id={favoriteTrack.id} previewURL={favoriteTrack.previewURL} />)}
         </div>
     )
 }
