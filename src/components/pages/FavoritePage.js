2c5eefe777a557b662c29260e56a9aa0eea4a0fa
Claire Kodia <claire.kodia@gmail.com>
2020-05-12 21:46:47 +0200

    Cleaned the code but left out two console.log to explain the code

--- src/components/pages/FavoritePage.js
+++ src/components/pages/FavoritePage.js
@@ -4,29 +4,30 @@ import axios from "axios"
 const FavoritePage = () => { 
     const [favoriteTrackList, setFavoriteTrackList] = useState([])
 
-    const getFavoriteTrackList = () =>{
+    const getFavoriteTrackList = () => {
         console.log("local storage token est", localStorage.getItem("token"))
-    if (localStorage.getItem("token")) {
-        axios.get("http://localhost:4242/favorite", {
-            headers: {
-            'x-access-token': localStorage.getItem("token"),
-            }
-        }).then(res => {
-            console.log("res.data est ", res.data);
-            console.log("res.status est ",res.status);
-            console.log("res.statusText est ",res.statusText);
-            console.log("res.headers est ",res.headers);
-            console.log("res.congi est ",res.config);
-            setFavoriteTrackList(res.data)
-        });
-    } else {
-        console.log("you need to connect")
-    }
+        if (localStorage.getItem("token")) {
+            axios.get("http://localhost:4242/favorite", {
+                headers: {
+                'x-access-token': localStorage.getItem("token"),
+                }
+            }).then(res => {
+                console.log("res.data est ", res.data);
+                setFavoriteTrackList(res.data)
+            });
+        } else {
+            alert("You need to connect")
+        }
     }
     
-    useEffect(getFavoriteTrackList(), [])
+    useEffect(() => getFavoriteTrackList(), [])
 
-    return <div>{favoriteTrackList.map(favoriteTrack => <p>{favoriteTrack.track_id}</p>)}</div>
+    return (
+        
+        <div>
+            {favoriteTrackList.map(favoriteTrack => <p>{favoriteTrack.track_id}</p>)}
+        </div>
+    )
 }
 
 export default FavoritePage;
