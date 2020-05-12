4d3e04013b5f396965f951e4b1594b26083478bb
Claire Kodia <claire.kodia@gmail.com>
2020-05-12 14:15:35 +0200

    Added an alert message to notify the user that the registration was successful

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, NavLink } from 'react-router-dom';
 import axios from "axios";
+
+import LoginModal from "./LoginModal"
 
 /* import NavbarHeader from '../shared/NavbarHeader';
 import NavbarFooter from '../shared/NavbarFooter'; */
@@ -9,14 +11,15 @@ const Register = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault() 
-        axios.post("http://localhost:4242/register", {
+        axios.post("http://localhost:4242/register/new_user", {
             username: e.target.username.value,
             password: e.target.password.value
         }).then(res => {
             console.log("resultat", res)
             if (res.status === 200) {
                 localStorage.setItem("token", res.headers["x-access-token"])
-                return <Redirect to={{ pathname: "/modepage-mainrules", username : e.target.username.value }} />
+                alert("Registration successful! Please sign in.")
+                return <Redirect to= "/"/>
             } else {
                 console.log("not possible, retry")
             }
@@ -27,11 +30,12 @@ const Register = () => {
     <div>
         {/* <NavbarHeader/>
                 <NavbarFooter/> */}
-        <h1>Sign in</h1>
+        <h1>Register</h1>
         <form onSubmit={handleSubmit}>
             <input type="text" name="username" />
             <input type="password" name="password" />
             <button type="submit">Register</button>
+            <NavLink to="/"><button type="submit">Cancel</button></NavLink>
         </form>
     </div>
     )
