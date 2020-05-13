55ae3fdfb379bc36a02f0f8f7ba5a71433473d6b
Claire Kodia <claire.kodia@gmail.com>
2020-05-13 22:17:06 +0200

    implemented the right links to the navlink

--- src/components/shared/NavbarFooter.js
+++ src/components/shared/NavbarFooter.js
@@ -19,7 +19,7 @@ class NavbarFooter extends Component {
         if (localStorage.getItem("token")) {
             this.setState({
                 loggedIn: !this.state.loggedIn
-              })
+            })
         }
     }
 
@@ -28,8 +28,8 @@ class NavbarFooter extends Component {
         return(
             <nav className="nav-bottom-container">
                 <ul className="nav-links-wrapper-footer">
-                    <li><NavLink className="nav-title" activeClassName ="current" to="/"><FontAwesomeIcon icon={faHome} className="nav-icon" /></NavLink></li>
-                    <li><NavLink className="nav-title" activeClassName ="current" to="/register/pseudo"><FontAwesomeIcon icon={faUser} className="nav-icon" /></NavLink></li>
+                    <li><NavLink className="nav-title" activeClassName ="current" to="/:pseudo"><FontAwesomeIcon icon={faHome} className="nav-icon" /></NavLink></li>
+                    <li><NavLink className="nav-title" activeClassName ="current" to={this.state.loggedIn? "/userpage" : "/authentication"}><FontAwesomeIcon icon={faUser} className="nav-icon" /></NavLink></li>
                     <li className={this.state.loggedIn? "LogOut_visible" : "LogOut_none"}><LogOut/></li> 
                     <li><NavLink className="nav-title" activeClassName ="current" to="/modepage-mainrules"><FontAwesomeIcon icon={faQuestionCircle} className="nav-icon" /></NavLink></li>
                 </ul>
