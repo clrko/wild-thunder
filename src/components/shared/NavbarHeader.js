f585ab465faac20f70710ea3e9798447499c0ea0
Claire Kodia <claire.kodia@gmail.com>
2020-05-13 16:16:06 +0200

    Imported the Log Out component into the Nav header

--- src/components/shared/NavbarHeader.js
+++ src/components/shared/NavbarHeader.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
 
+import LogOut from './LogOut';
 import MainLogo from "./MainLogo";
 import "./Navbar.css";
+
 
 class NavbarHeader extends Component {
     render(){
@@ -13,6 +15,7 @@ class NavbarHeader extends Component {
                     <li><NavLink className="nav-title" activeClassName ="current" to="/log_in/pseudo">Log In</NavLink></li> {/* must open the modal page */}
                     <li><NavLink className="nav-title" activeClassName ="current" to="/sign_in/pseudo">Sign In</NavLink></li>
                     <li><NavLink className="nav-title" activeClassName ="current" to="/contact/pseudo">Contact</NavLink></li>
+                    <li><LogOut/></li>
                 </ul>
             </nav>
         );
