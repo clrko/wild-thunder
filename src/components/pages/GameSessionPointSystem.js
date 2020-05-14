74d23100af818ce21c6fea784fb648aca292f2bc
SebG-prog <sebastien-gruet@orange.fr>
2020-05-14 19:16:53 +0200

    fix bug

--- src/components/pages/GameSessionPointSystem.js
+++ src/components/pages/GameSessionPointSystem.js
@@ -7,21 +7,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faStepForward } from '@fortawesome/free-solid-svg-icons';
 
-const GameSessionPointSystem = ({isArtistFound, score, validateAndChange, saveRoundAndLoadNextSong, counter}) => {
+const GameSessionPointSystem = ({ hidValidateButton, isArtistFound, score, validateAndChange, saveRoundAndLoadNextSong, counter }) => {
     return (
         <div className="scoreSystem">
+            {!hidValidateButton ?
+                <>
+                    {!isArtistFound ?
+                        (counter !== 0 ?
+                            <button onClick={validateAndChange} className="validate_button">
+                                <FontAwesomeIcon icon={faCheck} className="validate_icon" />
+                            </button>
+                            :
+                            <div>Too Late!</div>
+                        )
+                        : null}
+                    <button onClick={saveRoundAndLoadNextSong} className="next_button"><FontAwesomeIcon icon={faStepForward} className="next_icon" /></button>
+
+                </>
+                :
+                <div>Next song coming!</div>
+            }
             <p className="scoreSystem-p">Your score is {score} points</p>
-            {!isArtistFound ? (counter !== 0 ? 
-            <button onClick={validateAndChange} className="validate_button">
-                <FontAwesomeIcon icon={faCheck} className="validate_icon" />
-            </button>
-            :
-            <div>Too Late!</div>)
-            : null}
-            <button onClick={saveRoundAndLoadNextSong} className="next_button"><FontAwesomeIcon icon={faStepForward} className="next_icon" />
-            </button>
         </div>
     )
 }
-    
+
 export default GameSessionPointSystem; 
