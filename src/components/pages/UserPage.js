c6f8913ab547fc668b4f41119bcfc2ff79f47bb9
Claire Kodia <claire.kodia@gmail.com>
2020-05-14 08:32:01 +0200

    Finished the structure and imported the username from the databse

--- src/components/pages/UserPage.js
+++ src/components/pages/UserPage.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react"
+import React, { Component } from 'react'
+import axios from 'axios'
 
 import NavbarFooter from '../shared/NavbarFooter';
 import NavbarHeader from '../shared/NavbarHeader';
@@ -6,16 +7,45 @@ import NavbarHeader from '../shared/NavbarHeader';
 import './UserPage.css'
 
 class UserPage extends Component  {
+    state = {
+        loggedIn : false,
+        username: ""
+    }
+
+    getUsername() {
+        if (localStorage.getItem("token")) {
+            this.setState({
+                loggedIn: true
+            })
+            
+            axios.get("http://localhost:4242/auth", {
+                headers: {
+                    'x-access-token': localStorage.getItem("token"),
+                }
+            }).then(res => {
+                this.setState({
+                    username: res.data[0].username
+                })
+            });
+        } else {
+            this.setState({
+                loggedIn: false
+            })
+        }
+    }
+
+    componentDidMount() {
+        this.getUsername()
+    }
+
     render() {
         return (
         <div className="userpage-wrapper">
             <NavbarHeader />
-            <p>Hello we are on the userPage. we need import</p>
-            <ul>
-                <li>Username</li>
-                <li>Favorite List Card displaying the first 5 tracks and a more button that lead to the favortie List component</li>
-                <li>Scores per genre</li>
-            </ul>
+            <h1 className="userpage-title">Hi {this.state.username},</h1>
+            <h2 className="userpage-title-h2">Welcome to your profile</h2>
+            <h3 className="userpage-title-h3">Favorite tracks</h3>
+            <h3 className="userpage-title-h3">Achievements</h3>
             <NavbarFooter />
         </div>
         )
