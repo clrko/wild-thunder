6e08aeee7eb207ce2b25cdcfc6dcaf3fa6503ae4
Claire Kodia <claire.kodia@gmail.com>
2020-05-14 19:39:40 +0200

    Added the navFooter and navHeader to the artist similar and REco page

--- src/components/pages/EndSessionRank.js
+++ src/components/pages/EndSessionRank.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import axios from 'axios'
-import { NavLink } from 'react-router-dom'
 
 import NavbarFooter from '../shared/NavbarFooter';
 import NavbarHeader from '../shared/NavbarHeader';
