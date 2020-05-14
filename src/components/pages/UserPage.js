678562009557d60367fb9d9f31114a09a1ce58f4
Claire Kodia <claire.kodia@gmail.com>
2020-05-14 08:09:50 +0200

    Improted the navbars

--- src/components/pages/UserPage.js
+++ src/components/pages/UserPage.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react"
 
+import NavbarFooter from '../shared/NavbarFooter';
+import NavbarHeader from '../shared/NavbarHeader';
+
+import './UserPage.css'
 
 class UserPage extends Component  {
     render() {
-        return (<div>
+        return (
+        <div className="userpage-wrapper">
+            <NavbarHeader />
             <p>Hello we are on the userPage. we need import</p>
             <ul>
                 <li>Username</li>
                 <li>Favorite List Card displaying the first 5 tracks and a more button that lead to the favortie List component</li>
                 <li>Scores per genre</li>
             </ul>
-        </div>)
+            <NavbarFooter />
+        </div>
+        )
     }
 }
 
