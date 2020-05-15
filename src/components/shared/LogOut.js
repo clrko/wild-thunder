a504741e3970c21413112126a17d33777caf6d54
Claire Kodia <claire.kodia@gmail.com>
2020-05-15 08:44:22 +0200

    Added a cursor pointer to the log out button

--- src/components/shared/LogOut.js
+++ src/components/shared/LogOut.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 
+import './LogOut.css'
 
 const LogOut = () => {
     const [redirect, setRedirect] = useState(false)
